f2f2e579fb4a34b076a39528a548a2d907fdefe4
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-02-23 15:27:42 +0100

    Rearranged model classes

---
+++ src/model/levelType.js
@@ -0,0 +1,12 @@
+/**
+ * Created by olafjanssen on 23/02/14.
+ */
+
+var SetupType = {
+
+    ONE_BORDER: "one-border",
+    TWO_BORDERS: "two-borders",
+    FOUR_RANDOM: "four-random",
+    TRAITS_ONLY: "traits-only"
+
+};
