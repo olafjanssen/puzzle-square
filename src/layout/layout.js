9537fec67b12cc56714105adaa4ace60c9591432
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-02-26 00:18:03 +0100

    Grid styling

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -7,6 +7,7 @@ var LayoutManager = (function (eventBus) {
     eventBus.subscribe(Messages.NEW_GRID_NEEDED, function (data) {
         this.cols = data.cols;
         this.rows = data.rows;
+        this.traitDirections = data.traitDirections;
         updateLayout();
     });
 
@@ -16,30 +17,33 @@ var LayoutManager = (function (eventBus) {
         })
     });
 
-    var cols, rows;
+    var cols, rows, traitDirections;
 
     function updateLayout() {
-        var gridRatio = (this.cols + 2) / (this.rows + 2);
+        var ac = this.traitDirections[1] + this.traitDirections[3];
+        var ar = this.traitDirections[0] + this.traitDirections[2];
+
+        var gridRatio = (this.cols + ac) / (this.rows + ar);
         var clientRatio = document.body.clientWidth / document.body.clientHeight;
 
         var w, h, mw, mh;
         if (clientRatio < gridRatio) {
             mw = w = 0.9 * document.body.clientWidth;
-            if (gridRatio * w * (this.cols + 4) / (this.cols + 2) < document.body.clientHeight) {
+            if (gridRatio * w * (this.cols + ac + 2) / (this.cols + ac) < document.body.clientHeight) {
                 h = gridRatio * w;
-                mh = gridRatio * w * (this.cols + 4) / (this.cols + 2);
+                mh = gridRatio * w * (this.cols + ac + 2) / (this.cols + ac);
             } else {
-                h = 0.9 * document.body.clientHeight * (this.cols + 2) / (this.cols + 4);
+                h = 0.9 * document.body.clientHeight * (this.cols + ac) / (this.cols + ac + 2);
                 mh = 0.9 * document.body.clientHeight;
                 mw = w = gridRatio * h;
             }
         } else {
             mh = h = 0.9 * document.body.clientHeight;
-            if (gridRatio * h * (this.rows + 4) / (this.rows + 2) < document.body.clientWidth) {
+            if (gridRatio * h * (this.rows + ar+2) / (this.rows + ar) < document.body.clientWidth) {
                 w = gridRatio * h;
-                mw = gridRatio * w * (this.rows + 4) / (this.rows + 2);
+                mw = gridRatio * w * (this.rows + ar + 2) / (this.rows + ar);
             } else {
-                w = 0.9 * document.body.clientWidth * (this.rows + 2) / (this.rows + 4);
+                w = 0.9 * document.body.clientWidth * (this.rows + ar) / (this.rows + ar + 2);
                 mw = 0.9 * document.body.clientWidth;
                 mh = h = gridRatio * w;
             }
@@ -51,10 +55,10 @@ var LayoutManager = (function (eventBus) {
         style.marginRight = (-mw / 2) + "px";
 
         var stackStyle = document.getElementById("stack").style;
-        stackStyle.width = (w / (this.cols + 2)) + "px";
-        stackStyle.height = (h / (this.rows + 2)) + "px";
-        stackStyle.marginTop = (clientRatio < gridRatio ? (h / 2) - ((w / (this.cols + 2)) / 2) : -(w / (this.cols + 2)) / 2) + "px";
-        stackStyle.marginRight = (clientRatio >= gridRatio ? (w / 2) - ((h / (this.rows + 2)) / 2) : -(h / (this.rows + 2)) / 2) + "px";
+        stackStyle.width = (w / (this.cols + ac)) + "px";
+        stackStyle.height = (h / (this.rows + ar)) + "px";
+        stackStyle.marginTop = (clientRatio < gridRatio ? (h / 2) - ((w / (this.cols + ac)) / 2) : -(w / (this.cols + ac)) / 2) + "px";
+        stackStyle.marginRight = (clientRatio >= gridRatio ? (w / 2) - ((h / (this.rows + ar)) / 2) : -(h / (this.rows + ar)) / 2) + "px";
     }
 
 }(amplify));
