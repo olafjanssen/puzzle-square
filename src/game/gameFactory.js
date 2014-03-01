a71835ec68b705a59de71d380acfd7df598b718a
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-03-01 21:44:09 +0100

    Flipped around cards according to Eyal's wishes.

--- src/game/gameFactory.js
+++ src/game/gameFactory.js
@@ -47,13 +47,13 @@ var GameFactory = (function (eventBus) {
         eventBus.publish(Messages.NEW_TRAITS_CHOSEN, shuffle(gameSettings.deck.traitCardMap));
 
         var size = gameSettings.gridSize;
-        eventBus.publish(Messages.NEW_GRID_NEEDED, {cols: size[0], rows: size[1], traitDirections: [1, 1, 0, 0]});
+        eventBus.publish(Messages.NEW_GRID_NEEDED, {cols: size[0], rows: size[1], traitDirections: [1, 0, 0, 1]});
 
         for (var col = 0; col < size[0]; col++) {
             eventBus.publish(Messages.POSITION_FILLED, {col: col, row: -1});
         }
         for (var row = 0; row < size[1]; row++) {
-            eventBus.publish(Messages.POSITION_FILLED, {col: size[0], row: row});
+            eventBus.publish(Messages.POSITION_FILLED, {col: -1, row: row});
         }
     }
 
--- src/model/decks.js
+++ src/model/decks.js
@@ -2,8 +2,8 @@
 var deck1 = function () {
     return new Deck(
         [
-            [new Card({"color": "yellow"}), new Card({"color": "purple"}), new Card({"color": "red"}), new Card({"color": "blue"})],
-            [new Card({"shape": "butterfly"}), new Card({"shape": "cars"}), new Card({"shape": "flags"}), new Card({"shape": "doll"})]
+            [new Card({"shape": "butterfly"}), new Card({"shape": "cars"}), new Card({"shape": "flags"}), new Card({"shape": "doll"})],
+            [new Card({"color": "yellow"}), new Card({"color": "purple"}), new Card({"color": "red"}), new Card({"color": "blue"})]
         ],
         [   new Card({"color": "yellow", "shape": "butterfly"}),
             new Card({"color": "yellow", "shape": "cars"}),
