8cba75fc5cc54ff7bdd8a20f7d909bbce5d77e8b
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-03-12 23:18:39 +0100

    Add a guid user id

---
+++ service/service.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: olafjanssen
+ * Date: 3/12/14
+ * Time: 10:38 PM
+ */
+
+echo uniqe_id();
+
+?>
---
+++ src/state/stateManager.js
@@ -0,0 +1,27 @@
+/**
+ * Created by olafjanssen on 3/12/14.
+ */
+
+var StateManager = (function (eventBus) {
+
+    var uid;
+
+    eventBus.subscribe(Messages.UI_READY, function (data) {
+
+        // check hash for uid or create a new one and set the hash
+        uid = window.location.hash.length > 0 ? window.location.hash.substr(1) : guid();
+        window.location.hash = uid;
+
+    });
+
+    function guid() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+        });
+        return uuid;
+    };
+
+}(amplify));
